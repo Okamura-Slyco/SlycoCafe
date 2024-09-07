fda56679858587494fa955c682f3c49ea4fca685
Okamura-Slyco <marcos.okamura@slyco.com.br>
2024-09-07 13:03:15 -0300

    Revert "Refatoração do Código + GIF"
    This reverts commit 913bbc7f56d8839831c5a8d79bbf8ee608e9a304.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,6 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
-    id("kotlin-kapt")
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
 
-android {
-    namespace = "br.com.slyco.slycocafe"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "br.com.slyco.slycocafe"
-        minSdk = 26
-        //noinspection ExpiredTargetSdkVersion
-        targetSdk = 29
-        versionCode = 1
-        versionName = "1.1"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isDebuggable = false
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-        debug {
-            isDebuggable = true
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-    // Depedencias para utilizar o gif
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    kapt("com.github.bumptech.glide:compiler:4.12.0") // KAPT para processadores de anotação no Kotlin
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-}
