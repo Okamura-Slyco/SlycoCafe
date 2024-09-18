b17f5c1400f57901b7f926c0463344ee43eb5e83
Okamura-Slyco <marcos.okamura@slyco.com.br>
2024-09-18 20:03:50 -0300

    deixando display sempre ativo

--- app/src/main/java/br/com/slyco/slycocafe/DispenserProgress.kt
+++ app/src/main/java/br/com/slyco/slycocafe/DispenserProgress.kt
@@ -11,6 +11,7 @@ class DispenserProgress : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_dispenser_progress)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
