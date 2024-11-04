62c6f2b5025d52979d8953231d978bbd4bb5fada
Okamura-Slyco <marcos.okamura@slyco.com.br>
2024-11-04 00:04:18 -0300

    Create ESP32-Dispenser-v2.ino.ino
    Dispenser V2 fw

---
+++ ESP32/ESP32-Dispenser/ESP32-Dispenser-v2.ino/ESP32-Dispenser-v2.ino.ino
@@ -0,0 +1,258 @@
+//#include "BluetoothSerial.h"
+
+#include <Adafruit_PWMServoDriver.h>
+
+Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver();  // called this way, it uses the default address 0x40
+
+#define INIT_CLOSE 1
+
+#define RELE0 0
+#define RELE1 1
+#define RELE2 2
+#define RELE3 3
+
+#define MODULO_LED1 RELE0
+#define MODULO_LED2 RELE1
+
+#define MODULO_VCC RELE2
+
+#define LED_ON 0
+#define LED_OFF 1
+
+#define DISPENSER_QTY 6
+#define DISPENSER_STOCK 50
+
+#define PCA9865_OSCILLATOR_FREQUENCY 27000000
+#define SERVO_PWM_FREQUENCY 50
+
+
+#define SERVO_MIN_PULSE 500
+#define SERVO_MAX_PULSE 2500
+
+#define LED_MIN_PULSE 0
+#define LED_MAX_PULSE 1000000/SERVO_PWM_FREQUENCY
+
+#define DELAY_BLINK 500
+#define DELAY_SERVO 500
+#define DELAY_SERVO_STEP 30
+
+#define RESET_RELEASE 'memset (release, 0x00, sizeof(release))'
+
+#define DISPENSER(X) (1 << X)
+#define DISPENSER_PWM(X) (X)
+#define DISPENSER_A 5
+#define DISPENSER_B 4
+#define DISPENSER_C 3
+#define DISPENSER_D 2
+#define DISPENSER_E 1
+#define DISPENSER_F 0
+
+#define CONTROL_DOOR 1
+#define RELEASE_DOOR 0
+
+#define DRAWER_REST 180
+#define DRAWER_PUSH 0
+#define MAX_SERVO_ANGLE() max(DRAWER_REST,DRAWER_PUSH)
+#define MIN_SERVO_ANGLE() min(DRAWER_REST,DRAWER_PUSH)
+
+#define ROTATION_STEP_ANGLE 10
+
+String device_name = "Slyco Dispenser Monitor";
+
+// Check if Bluetooth is available
+//#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+//#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+//#endif
+
+// Check Serial Port Profile
+//#if !defined(CONFIG_BT_SPP_ENABLED)
+//#error Serial Port Profile for Bluetooth is not available or not enabled. It is only available for the ESP32 chip.
+//#endif
+
+//Modulo A primeiro da esquerda para a direita olha por traz do dispenser
+//PS porta saida, libera o café para o cliente
+//PC porta controle, trava o cafe, para a próxima liberação.
+//BluetoothSerial SerialBT;
+
+char Comando;
+
+const int BUFFER_SIZE = DISPENSER_STOCK * DISPENSER_QTY;
+char buf[BUFFER_SIZE];
+
+char release[DISPENSER_STOCK];
+
+int servoPosition[DISPENSER_QTY] = {DRAWER_REST,DRAWER_REST,DRAWER_REST,DRAWER_REST,DRAWER_REST,DRAWER_REST};
+
+void setup() {
+  pinMode(MODULO_LED1, OUTPUT);
+  pinMode(MODULO_LED2, OUTPUT);
+  pinMode(MODULO_VCC, OUTPUT);
+
+  digitalWrite(MODULO_LED1, LED_ON);
+  digitalWrite(MODULO_LED2, LED_ON);
+  digitalWrite(MODULO_VCC, LED_ON);
+  RESET_RELEASE;
+
+  //servoPosition
+
+  Serial.begin(9600);
+//  SerialBT.begin(device_name);
+//  SerialBT.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+
+  board1.begin();
+  board1.setOscillatorFrequency(PCA9865_OSCILLATOR_FREQUENCY);
+  board1.setPWMFreq(SERVO_PWM_FREQUENCY);  // Analog servos run at ~60 Hz updates
+
+  release_items("Z\n", 2);
+
+  digitalWrite(MODULO_LED2, LED_OFF);
+  digitalWrite(MODULO_VCC, LED_OFF);
+}
+
+
+void loop() {
+  if (Serial.available() > 0) {
+    // read the incoming bytes:
+    int rlen = Serial.readBytesUntil('\n', buf, BUFFER_SIZE);
+    release_items(buf, rlen);
+  }
+  delay(DELAY_SERVO);
+}
+
+void release_items(char* buffer, int qty) {
+  int i;
+
+  int int_releases = 0;
+  int releases = 0;
+  int item;
+
+
+  for (i = 0; i < DISPENSER_STOCK; i++) release[i] = 0;
+
+  for (i = 0; i < qty; i++) {
+    switch (buffer[i]) {
+      case 'A':
+      case 'B':
+      case 'C':
+      case 'D':
+      case 'E':
+      case 'F':
+        //case 'Z':
+        item = buffer[i] - 0x40;
+        int_releases = 0;
+
+        while (0 != (release[int_releases] & (0x01 << (item - 1)))) {
+          int_releases++;
+        }
+        release[int_releases] |= 0x01 << (item - 1);
+
+
+        if (int_releases > releases) releases = int_releases;
+
+        break;
+      case 'z':
+        digitalWrite(MODULO_VCC, LED_ON);
+        SetDoorState(DISPENSER(DISPENSER_A) | DISPENSER(DISPENSER_B) | DISPENSER(DISPENSER_C) | DISPENSER(DISPENSER_D) | DISPENSER(DISPENSER_E) | DISPENSER(DISPENSER_F), DRAWER_PUSH);
+        delay(DELAY_SERVO);
+        digitalWrite(MODULO_VCC, LED_OFF);
+        return;
+      case 'Z':
+        digitalWrite(MODULO_VCC, LED_ON);
+        SetDoorState(DISPENSER(DISPENSER_A) | DISPENSER(DISPENSER_B) | DISPENSER(DISPENSER_C) | DISPENSER(DISPENSER_D) | DISPENSER(DISPENSER_E) | DISPENSER(DISPENSER_F), DRAWER_REST);
+        delay(DELAY_SERVO);
+        digitalWrite(MODULO_VCC, LED_OFF);
+        return;
+      default:
+        break;
+    }
+  }
+
+  digitalWrite(MODULO_VCC, LED_ON);
+
+  Serial.print("Ri");
+  Serial.print(int_releases, DEC);
+  Serial.print("\n");
+  for (item = 0; release[item] && (item <= int_releases) && (item < DISPENSER_STOCK); item++) {
+    Serial.print("ri");
+    Serial.print(item, DEC);
+    Serial.print(":");
+    Serial.print(release[item], BIN);
+    Serial.print("\n");
+
+    Release(release[item]);
+
+    release[item] = 0x00;
+    Serial.print("rf");
+    Serial.print(item, DEC);
+    Serial.print("\n");
+  }
+  Serial.print("Rf");
+  Serial.print(int_releases, DEC);
+  Serial.print("\n");
+
+  digitalWrite(MODULO_VCC, LED_OFF);
+}
+
+void Release(char comando) {
+  int door = 0;
+  digitalWrite(MODULO_LED1, LED_OFF);
+  digitalWrite(MODULO_LED2, LED_ON);
+
+  SetDoorState(comando, DRAWER_PUSH);
+  
+  delay(DELAY_SERVO);
+
+  SetDoorState(comando, DRAWER_REST);
+
+  delay(DELAY_SERVO);
+  
+  digitalWrite(MODULO_LED1, LED_ON);
+  digitalWrite(MODULO_LED2, LED_OFF);
+}
+
+uint16_t angleToMicroseconds(int ang)  //gets angle in degree and returns the pulse width
+{
+  uint16_t pulse = (uint16_t)map(ang, DRAWER_PUSH, DRAWER_REST, SERVO_MIN_PULSE, SERVO_MAX_PULSE);  // map angle of 0 to 180 to Servo min and Servo max
+  return pulse;
+}
+
+uint16_t angleToLedPWM(int ang)  //gets angle in degree and returns the pulse width
+{
+  uint16_t pulse = (uint16_t)map(ang, DRAWER_PUSH, DRAWER_REST, LED_MAX_PULSE, LED_MIN_PULSE );  // map angle of 0 to 180 to Servo min and Servo max
+  return pulse;
+}
+
+void SetDoorState(int doors, int angle) {
+  int i;
+
+  int myAngle = 0;
+  int myStep = 0;
+
+  for (i = 0; i < DISPENSER_QTY; i++) {
+    if (doors & (0x01 << i)) {
+      myAngle = servoPosition[i];
+      break;
+    }
+  }
+
+  if (myAngle > angle) myStep = -1*ROTATION_STEP_ANGLE;
+  else myStep = ROTATION_STEP_ANGLE;
+
+  while (myAngle != angle)
+  {
+    myAngle += myStep;
+    if (myAngle > MAX_SERVO_ANGLE()) myAngle = MAX_SERVO_ANGLE();
+    else if (myAngle < MIN_SERVO_ANGLE()) myAngle = MIN_SERVO_ANGLE();
+
+    for (i = 0; i < DISPENSER_QTY; i++) {
+      if (doors & (0x01 << i)) {
+        uint16_t ledPwm = angleToLedPWM(myAngle);
+        board1.writeMicroseconds(DISPENSER_PWM(i), angleToMicroseconds(myAngle));
+        board1.writeMicroseconds(DISPENSER_PWM(i)+8, ledPwm);
+        servoPosition[i] = myAngle;
+      }
+    }
+
+    delay(DELAY_SERVO_STEP);
+  }
+}
